080f42b3f765081bb25c56d8f9f5f57b226133eb
Kate Pond <kate@thepondsedge.com>
2018-05-06 11:25:31 -0700

    partial solution

--- lib/matrix_check_sum.rb
+++ lib/matrix_check_sum.rb
@@ -3,5 +3,22 @@
 # of numbers in row i is the same as the sum of numbers in column i for i = 0 to row.length-1
 # If this is the case, return true. Otherwise, return false.
 def matrix_check_sum(matrix)
-  raise NotImplementedError
+
 end
+
+# Two methods to create an array for sums of rows and sums of columns
+
+row_arr = []
+row = 0
+while row < matrix.length
+  row_sum = 0
+  col = 0
+  while col < matrix[row].length
+    row_sum += matrix[row][col]
+    col += 1
+  end
+  row_arr << row_sum
+  row += 1
+end
+
+# matrix.map {|row| row.sum}
